32f5442334a3d6e282d7e976615e9d44badba2a5
hadeelaboelrish <hadeel.taha.246369@gmail.com>
2026-01-01 16:15:25 +0200

    Rename function to extract_carts and adjust logic

--- script/extract.py
+++ script/extract.py
@@ -5,13 +5,13 @@ from script.logger import logger
 from script.config import API_URL, RAW_DATA_PATH
 
 
-def extract_products():
+def extract_carts():
   try:
     logger.info('Starting Data Extraction')
     response = requests.get(API_URL)
     response.raise_for_status()
-    data = response.json()
-    file_name = f'products_{datetime.now().date()}.json'
+    data = response.json()['carts']
+    file_name = f'carts_{datetime.now().date()}.json'
     file_path = RAW_DATA_PATH + file_name
     with open(file_path, 'w') as f:
       json.dump(data, f)
