45ae756cb96112fff8cbc8eee2a33689096e3dcc
hadeelaboelrish <hadeel.taha.246369@gmail.com>
2026-01-01 15:10:15 +0200

    Add configuration settings for API and database

--- script/config.py
+++ script/config.py
@@ -1,1 +1,12 @@
+API_URL = "https://fakestoreapi.com/products"
 
+RAW_DATA_PATH = "data/raw/"
+PROCESSED_DATA_PATH = "data/processed/"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "dwh",
+    "user": "postgres",
+    "password": "postgres"
+}
